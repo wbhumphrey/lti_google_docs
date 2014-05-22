26dfb10ac717ea7ca96bd98a5e5ec986d8dfa1a7
Ryan Babiuch <pympnotiq@gmail.com>
2014-05-22 08:34:21 -0500

    Wired up AngularJS to main view. - Added require angular directive to applicaiton.js - Created LTI_GOOGLE_DOCS angularjs module in main.js - Create MainCtrl in main.js - Added markup to index.erb to include module and use controller.

--- app/assets/javascripts/lti_google_docs/application.js
+++ app/assets/javascripts/lti_google_docs/application.js
@@ -9,5 +9,5 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+//= require angular
 //= require_tree .
---
+++ app/assets/javascripts/lti_google_docs/main.js
@@ -0,0 +1,5 @@
+var app = angular.module('LTI_GOOGLE_DOCS', []);
+
+app.controller('MainCtrl', function($scope) {
+    $scope.items = ["one", "two", "three"];
+});
