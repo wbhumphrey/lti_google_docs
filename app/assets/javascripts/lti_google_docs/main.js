fc5924a9e7a896a5e89f97b67f628eb63bbc4295
Ryan Babiuch <pympnotiq@gmail.com>
2014-05-22 10:14:45 -0500

    Added rudimentary UI.

--- app/assets/javascripts/lti_google_docs/main.js
+++ app/assets/javascripts/lti_google_docs/main.js
@@ -2,4 +2,8 @@ var app = angular.module('LTI_GOOGLE_DOCS', []);
 
 app.controller('MainCtrl', function($scope) {
     $scope.items = ["one", "two", "three"];
+
+    $scope.clicky = function() {
+        console.log("You clicked the button!");
+    };
 });
