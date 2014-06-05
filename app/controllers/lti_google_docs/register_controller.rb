3d42034e5b0f54dd0ff0f778a2f82c3bf2f3a3d9
Ryan Babiuch <pympnotiq@gmail.com>
2014-06-05 09:03:20 -0500

    updating exchange_code with grant_type.

--- app/controllers/lti_google_docs/register_controller.rb
+++ app/controllers/lti_google_docs/register_controller.rb
@@ -136,7 +136,7 @@ module LtiGoogleDocs
       client.authorization.client_secret = CLIENT_SECRET
       client.authorization.code = authorization_code
       client.authorization.redirect_uri = REDIRECT_URI
-
+      client.authorization.grant_type = 'authorization_code'
       begin
         client.authorization.fetch_access_token!
         return client.authorization
