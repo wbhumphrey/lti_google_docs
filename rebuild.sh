6dc9d1a232f1f97acc20c6f38568d2f595f4a5ad
Ryan Babiuch <pympnotiq@gmail.com>
2014-06-25 11:21:29 -0500

    Cleaning up workspace a bit.

--- rebuild.sh
+++
@@ -1,7 +0,0 @@
-git commit -a && git push origin google-api-integration
-
-cd /Users/Ryan/RubymineProjects/HerokuThrowaway
-
-bundle update && bundle install && git commit -a && git push heroku master
-
-cd /Users/Ryan/Development/lti_google_docs
