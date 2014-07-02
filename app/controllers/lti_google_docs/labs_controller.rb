277e88b0b9f9ce01eb1bf5d77929355ed1302946
Ryan Babiuch <pympnotiq@gmail.com>
2014-07-02 16:00:35 -0500

    removed unnecessary whitespace.

--- app/controllers/lti_google_docs/labs_controller.rb
+++ app/controllers/lti_google_docs/labs_controller.rb
@@ -12,7 +12,6 @@ module LtiGoogleDocs
           @access_token = session[:google_access_token]
           @canvas_access_token = session[:canvas_access_token]
           render template: 'lti_google_docs/launch/error', tp: tool_provider if tool_provider.lti_msg
-          
       end
 
       def start
@@ -32,7 +31,6 @@ module LtiGoogleDocs
               render :index
           end
       end
-      
       
       def all
 #          puts request.inspect
@@ -65,22 +63,15 @@ module LtiGoogleDocs
               puts "LAB NAMED: #{existing_lab.title} ALREADY EXISTS!"
               render text: "LAB NOT CREATED!";
           end
-          
       end
       
       def remove
-      
-          puts "INSIDE REMOVE!"
-#          puts params.inspect
-      
           lab_id = params[:id]
           puts "DELETING LAB WITH ID: #{lab_id}"
           
           lab = Lab.find_by(id: lab_id)
           lab.destroy
-          
-          
-          
+ 
           render text: "ok"
       end
   end
