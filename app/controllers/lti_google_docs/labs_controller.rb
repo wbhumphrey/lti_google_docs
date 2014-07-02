39548bde8bb71ad1d1295901abafaf6424fa6135
Ryan Babiuch <pympnotiq@gmail.com>
2014-07-02 16:01:18 -0500

    removed commented out debug puts.

--- app/controllers/lti_google_docs/labs_controller.rb
+++ app/controllers/lti_google_docs/labs_controller.rb
@@ -7,8 +7,7 @@ module LtiGoogleDocs
       #GET /labs
       def index
           puts "INSIDE LABS INDEX"
-#          obj = YAML.load_file(File.join(__dir__,'../../../', 'config.yml'))
-#          puts obj.inspect
+
           @access_token = session[:google_access_token]
           @canvas_access_token = session[:canvas_access_token]
           render template: 'lti_google_docs/launch/error', tp: tool_provider if tool_provider.lti_msg
@@ -16,8 +15,7 @@ module LtiGoogleDocs
 
       def start
           puts "INSIDE LAB START!"
-#          puts request.inspect
-          
+
           puts params.inspect
           session[:userid] = params[:user_id]
           session[:current_course_id] = params["custom_canvas_course_id"]
@@ -33,7 +31,6 @@ module LtiGoogleDocs
       end
       
       def all
-#          puts request.inspect
           puts "CANVAS ACCESS TOKEN FROM SESSION: #{session[:canvas_access_token]}"
           puts "CANVAS ACCESS TOKEN FROM CLIENT: #{canvas_client.access_token}"
           puts "MY USER ID FROM SESSION: #{session[:userid]}"
@@ -46,8 +43,7 @@ module LtiGoogleDocs
       #POST /labs/new
       def create
           puts "INSIDE CREATE"
-#          puts params.inspect
-          
+
           existing_lab = Lab.find_by(title: params[:title])
           if !existing_lab
               title = params[:title]
