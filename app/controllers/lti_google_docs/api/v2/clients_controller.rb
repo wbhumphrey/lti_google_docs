9ccc57c08586da59c7fca14d89d6ba4251ae0d5b
Ryan Babiuch <pympnotiq@gmail.com>
2014-09-23 16:26:59 -0500

    fixed typo: Clients -> Client

--- app/controllers/lti_google_docs/api/v2/clients_controller.rb
+++ app/controllers/lti_google_docs/api/v2/clients_controller.rb
@@ -89,7 +89,7 @@ module LtiGoogleDocs::Api::V2
             if !lti_user
                 #tell the client to retrieve the canvas token from the canvas server
                 
-                @oauth_consumer_key = Clients.find_by(client_id: params[:oauth_consumer_key]).canvas_clientid
+                @oauth_consumer_key = Client.find_by(client_id: params[:oauth_consumer_key]).canvas_clientid
                 @canvas_user_id = params[:custom_canvas_user_id]
                 @canvas_server_address = params[:custom_canvas_api_domain]
                 render "retrieve_canvas_token"
